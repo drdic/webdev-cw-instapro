1e63519f09311803c395a898f6a1d1685bd13290
drdic <drdic26@gmail.com>
2026-07-28 22:49:57 +0300

    Исправление LoadingPageComponent

--- components/loading-page-component.js
+++ components/loading-page-component.js
@@ -1,114 +1,19 @@
-export function getPostsByUser({ token, userId }) {
-  return fetch(`${postsHost}/user-posts/${userId}`, {
-    method: "GET",
-    headers: {
-      Authorization: token,
-  })
-    .then((response) => {
-    })
-    .then((data) => {
-      return data.posts;
-    });
-}
+import { renderHeaderComponent } from "./header-component.js";
 
-export function likePost({ token, postId }) {
-  return fetch(`${postsHost}/${postId}/like`, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-    },
-  })
-    .then((response) => {
-      if (response.status === 401) {
-        throw new Error("Нет авторизации");
-      }
-      if (response.status === 400) {
-        throw new Error("Нельзя лайкнуть свой пост");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      return data.post;
-    });
-}
+export function renderLoadingPageComponent({ appEl, user, goToPage }) {
+  const appHtml = `
+    <div class="page-container">
+      <div class="header-container"></div>
+      <div class="loading-page">
+        <div class="loader"><div></div><div></div><div></div></div>
+      </div>
+    </div>`;
 
-export function dislikePost({ token, postId }) {
-  return fetch(`${postsHost}/${postId}/dislike`, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-    },
-  })
-    .then((response) => {
-      if (response.status === 401) {
-        throw new Error("Нет авторизации");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      return data.post;
-    });
-}
+  appEl.innerHTML = appHtml;
 
-export function addPost({ token, description, imageUrl }) {
-  return fetch(postsHost, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-    },
-    body: JSON.stringify({
-      description,
-      imageUrl,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Некорректные данные");
-    }
-    return response.json();
-  });
-}
-
-export function registerUser({ login, password, name, imageUrl }) {
-  return fetch(baseHost + "/api/user", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-      name,
-      imageUrl,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Такой пользователь уже существует");
-    }
-    return response.json();
-  });
-}
-
-export function loginUser({ login, password }) {
-  return fetch(baseHost + "/api/user/login", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Неверный логин или пароль");
-    }
-    return response.json();
-  });
-}
-
-// Загружает картинку в облако, возвращает url загруженной картинки
-export function uploadImage({ file }) {
-  const data = new FormData();
-  data.append("file", file);
-
-  return fetch(baseHost + "/api/upload/image", {
-    method: "POST",
-    body: data,
-  }).then((response) => {
-    return response.json();
+  renderHeaderComponent({
+    user,
+    element: document.querySelector(".header-container"),
+    goToPage,
   });
 }
